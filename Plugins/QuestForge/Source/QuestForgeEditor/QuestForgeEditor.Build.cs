3ab6b4a16e3855698c74da8e5a04731f5522f6b6
BankShotZombies <60952462+BankShotZombies@users.noreply.github.com>
2026-03-04 06:50:54 -0800

    Initial commit

---
+++ Plugins/QuestForge/Source/QuestForgeEditor/QuestForgeEditor.Build.cs
@@ -0,0 +1,25 @@
+using UnrealBuildTool;
+
+public class QuestForgeEditor : ModuleRules
+{
+	public QuestForgeEditor(ReadOnlyTargetRules Target) : base(Target)
+	{
+		PCHUsage = ModuleRules.PCHUsageMode.UseExplicitOrSharedPCHs;
+        
+		PublicDependencyModuleNames.AddRange(new string[]
+		{
+			"Core",
+			"QuestForgeRuntime"
+		});
+        
+		PrivateDependencyModuleNames.AddRange(new string[]
+		{
+			"CoreUObject",
+			"Engine",
+			"Slate",
+			"SlateCore",
+			"UnrealEd",
+			"EditorFramework"
+		});
+	}
+}
